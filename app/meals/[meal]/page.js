881e1ba5cac0b4bd8bee59f09141534d1ae71a9f
yerish14 <yerisagui@gmail.com>
2024-04-18 14:27:58 -0700

    configuring meals pages and bd

--- app/meals/[meal]/page.js
+++ app/meals/[meal]/page.js
@@ -1,9 +1,8 @@
-import Link from "next/link";
-import classes from "./page.module.css";
 
-function Meal({ params }) {
-  return (
-    <div></div>
-  );
+
+
+function Meal() {
+  return <>
+  </>
 }
 export default Meal;
