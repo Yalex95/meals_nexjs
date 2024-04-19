f00b96099abea3bc1f85ea2fc4baa96b47c2e38e
yerish14 <yerisagui@gmail.com>
2024-04-18 17:50:46 -0700

    meals details update

---
+++ app/meals/not-found.js
@@ -0,0 +1,9 @@
+import Link from "next/link";
+
+export default  function NotFound(){
+  return <main className="not-found">
+    <h1>404</h1>
+    <h1>Meal Not found</h1>
+    <p>Unfortunately, we couldn't find the requested Meal.</p>
+  </main>
+}
