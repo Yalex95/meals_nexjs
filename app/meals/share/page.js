dae9a6ba64f7439d5f1d142bf0c343c9e506cb4a
yerish14 <yerisagui@gmail.com>
2024-04-26 10:37:33 -0700

    added react hooks for form handleling

--- app/meals/share/page.js
+++ app/meals/share/page.js
@@ -1,10 +1,12 @@
 'use client'
-import { shareMeal } from '@/lib/actions';
-import ImagePicker from '../../../components/meals/image-picker';
-import classes from './page.module.css';
+import { useFormState } from "react-dom";
+import { shareMeal } from "@/lib/actions";
+import ImagePicker from "../../../components/meals/image-picker";
+import classes from "./page.module.css";
+import MealsFormSubmit from "@/components/meals/meals-form-submit";
 
 export default function ShareMealPage() {
-  
+  const [state, formAction]=useFormState(shareMeal, { message: null });
   return (
     <>
       <header className={classes.header}>
@@ -14,7 +16,7 @@ export default function ShareMealPage() {
         <p>Or any other meal you feel needs sharing!</p>
       </header>
       <main className={classes.main}>
-        <form className={classes.form} action={shareMeal}>
+        <form className={classes.form} action={formAction}>
           <div className={classes.row}>
             <p>
               <label htmlFor="name">Your name</label>
@@ -42,9 +44,10 @@ export default function ShareMealPage() {
               required
             ></textarea>
           </p>
-          <ImagePicker label="your image" name="image"/>
+          <ImagePicker label="your image" name="image" />
+          {state.message && <p>{state.message}</p>}
           <p className={classes.actions}>
-            <button type="submit">Share Meal</button>
+            <MealsFormSubmit />
           </p>
         </form>
       </main>
